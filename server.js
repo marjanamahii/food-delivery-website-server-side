b0877255da2cb768dd85f2a47725896bb2f15b0a
marjanamahii <marjanamai777@gmail.com>
2021-10-29 03:48:33 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yt3ul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Food delivery server is running');
+});
+app.listen(port, () => {
+    console.log('Running food deliver server running at port', port);
+})
